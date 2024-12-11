fa1a94c531aafa549e8ee3a6dea16150da5e7cfa
rudalkunwar <ruzalkunwar@gmail.com>
2024-12-11 18:41:57 +0545

    welcome email notification

--- database/migrations/2024_11_24_132006_create_discussions_table.php
+++ database/migrations/2024_11_24_132006_create_discussions_table.php
@@ -14,10 +14,9 @@ return new class extends Migration
         Schema::create('discussions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('course_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('student_id')->constrained();
             $table->string('title');
             $table->text('content');
-            $table->foreignId('parent_id')->nullable()->constrained('discussions');
             $table->timestamps();
         });
     }
